5063c296f16a808e14da5b0ddbdee8aedec15e4c
drahul540 <drahul540@gmail.com>
2024-08-01 20:26:39 +0530

    add history.list()

--- src/utils/gmail/email.js
+++ src/utils/gmail/email.js
@@ -3,13 +3,16 @@ const {oAuth2Client} = require('./auth');
 
 function getEmailDetails(messageId, callback) {
     const gmail = google.gmail({version: 'v1', auth: oAuth2Client});
-    gmail.users.messages.get({
-        userId: 'me',
-        id: messageId
-    }, (err, res) => {
-        if (err) return console.error('Error getting email details:', err);
-        callback(res.data);
-    });
+    gmail.users.history.list((err, resp)=>{
+        gmail.users.messages.get({
+            userId: 'me',
+            id: messageId
+        }, (err, res) => {
+            if (err) return console.error('Error getting email details:', err);
+            callback(res.data);
+        });
+    })
+    
 }
 
 module.exports = { getEmailDetails };
